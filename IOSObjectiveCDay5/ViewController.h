80da609067ec736e6debc628ebc8f9c61c73e2ad
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-04-18 09:17:24 +0200

    Initial Commit

---
+++ IOSObjectiveCDay5/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  IOSObjectiveCDay5
+//
+//  Created by Sara Talat on 18/04/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSObjectiveCDay5Tests/IOSObjectiveCDay5Tests.m
@@ -0,0 +1,36 @@
+//
+//  IOSObjectiveCDay5Tests.m
+//  IOSObjectiveCDay5Tests
+//
+//  Created by Sara Talat on 18/04/2024.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IOSObjectiveCDay5Tests : XCTestCase
+
+@end
+
+@implementation IOSObjectiveCDay5Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
